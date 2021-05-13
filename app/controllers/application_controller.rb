bc0a50bb69cb69418d6e2c4d57bc9e67a4b555d7
jupcan <jpereac@gmail.com>
2021-05-13 21:56:13 +0200

    minor code refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActionController::ParameterMissing, with: :render_bad_request_response
+
+    def render_unprocessable_entity_response(exception)
+        render json: exception.record.errors, status: :unprocessable_entity
+    end
+
+    def render_not_found_response(exception)
+        render json: { error: exception.message }, status: :not_found
+    end
+
+    def render_bad_request_response(exception)
+        render json: { error: exception.message }, status: :bad_request
+    end
 end
